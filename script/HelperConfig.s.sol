45213b15ce774430c72e6ff7edaa148f18f406f6
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-24 12:34:27 +0530

    feat: refactor DeployFundMe to use dynamic price feed from HelperConfig

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    NetworkConfig public activeNetworkConfig; // variable to store the active network configuration
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // Sepolia
+            activeNetworkConfig = getSelpoliaETHConfig();
+        }
+        {
+            // Anvil
+            activeNetworkConfig = getAnvilETHConfig();
+        }
+    }
+
+    function getSelpoliaETHConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory selpoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return selpoliaConfig;
+    }
+
+    function getAnvilETHConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+
+        vm.stopBroadcast();
+    }
+
+    // a getter function for priceFeed
+    function getPriceFeed() public view returns (address) {
+        return activeNetworkConfig.priceFeed;
+    }
+}
