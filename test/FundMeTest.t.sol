f0bd85bb493f4f43bd6218ed94973946d16c8755
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-24 12:11:20 +0530

    feat: update FundMeTest to use DeployFundMe for contract deployment and add price feed version test; create command file for testing

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,16 +3,23 @@ pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testOwnerIsMsgSender() external view {
         // check if the owner is the sender
-        assertEq(fundMe.getOwner(), address(this));
+        assertEq(fundMe.getOwner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsCorrect() external view {
+        // check if the price feed version is correct
+        assertEq(fundMe.getPriceFeedVersion(), 4);
     }
 }
