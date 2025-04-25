037309fa94fcf79c236fb2e5908e837f78e0550f
kavinda-100 <kavindarathnayake100@gmail.com>
2025-04-25 11:48:39 +0530

    feat: add foundry-devops submodule and update settings for remappings

---
+++ script/Interations.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {
+    function run() external {}
+}
+
+contract WithdrawFundMe is Script {}
